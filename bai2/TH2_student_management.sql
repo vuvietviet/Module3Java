086a710896dde750d6f657f4b27f28571071d5ff
vu viet <vvviet97@gmail.com>
2022-06-16 16:53:34 +0700

    bai2BTUpdate

--- bai2/TH2_student_management.sql
+++ bai2/TH2_student_management.sql
@@ -1,1 +1,9 @@
-SELECT * FROM student_management.class;
+SELECT * FROM student_management.student;
+create table class(
+id varchar(50),
+name varchar(45));
+create table teacher(
+id varchar(50),
+name varchar(45),
+age int,
+country varchar(45));
