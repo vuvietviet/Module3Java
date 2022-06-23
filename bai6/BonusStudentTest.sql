13bcc55542d38e7985f141e1feafbb585d755a3e
vu viet <vvviet97@gmail.com>
2022-06-23 11:42:52 +0700

    'BonusStudentTest_Update'

--- bai6/BonusStudentTest.sql
+++ bai6/BonusStudentTest.sql
@@ -51,7 +51,7 @@ join Test t on t.testID = st.testID
 group by t.TName
 order by t.TName;
 -- cau 8
-select s.SName,t.TName from Student s
+select s.SName,if(t.TName is null,'NULL',t.TName) from Student s
 left join StudentTest st on s.RN = st.RN
 left join Test t on t.testID = st.testID;
 -- cau 9 
@@ -61,6 +61,7 @@ where RN >0;
 alter table Student 
 add column status varchar(10);
 -- cau 11
+	-- cách 1:
 update Student
 set status = 'Young'
 where RN > 0 and age < 30;
@@ -68,7 +69,10 @@ where RN > 0 and age < 30;
 update Student
 set status = 'Old'
 where RN > 0 and age >= 30;
-
+select * from student;
+	-- cách 2:
+update Student 
+set status = if(age < 30,'Young','Old') where RN > 0;
 select * from student;
 -- cau 12
 create view vwStudentTest as
@@ -79,12 +83,40 @@ order by st.STDate;
 
 select * from vwStudentTest;
 -- cau 13 chưa xong
+	-- cách 1: lưu thay đổi ra bảng riêng
 create table Student_temp(
 RN int primary key,
 SName varchar(20),
 Age tinyint,
 Status varchar(10)
 );
+
+DELIMITER //
+ DROP PROCEDURE IF EXISTS `checkLogin`//
+ CREATE PROCEDURE `checkLogin`(
+    IN input_age int,
+    OUT result VARCHAR(255)
+)
+BEGIN
+    /*Bien flag luu tru age. Mac dinh la -1*/
+    DECLARE flag INT DEFAULT -1;
+     
+    /*Thuc hien truy van gan age vao bien flag*/
+    SELECT age INTO flag FROM student
+    WHERE age = input_age;
+ 
+    /*Sau khi thuc hien lenh select nay ma ko co du lieu thi
+      luc nay flag se khong thay doi. Chinh vi the neu flag = -1 tuc la sai thong tin
+    */
+    IF (flag <= 0) THEN
+            SET result = 'Dang nhap sai';
+        ELSEIF (flag < 30) THEN
+            SET result = 'Young';
+        ELSE
+            SET result = 'Old';
+    END IF;
+END
+ //DELIMITER ;
 
 DROP TRIGGER IF EXISTS tgSetStatus;
 DELIMITER //
@@ -93,13 +125,12 @@ after UPDATE ON student
 FOR EACH ROW
 
 BEGIN
-
+ call checkLogin(new.age,@result);
  INSERT INTO Student_temp
  set RN = old.RN,
  SName  = old.SName,
  Age = new.Age,
- Status = old.Status if
- -- cần tạo 1 procedure để check giá trị 
+ Status = (select@result);
 END;
 //DELIMITER ;
 
@@ -107,4 +138,51 @@ update Student set age = 32
 where RN = 1;
 select * from Student;
 select * from Student_temp;
--- cau 14 chưa xong
+	-- cách 2: thay đổi luôn trên bảng cơ sở
+DROP TRIGGER IF EXISTS tgSetStatus_1;
+DELIMITER //
+CREATE TRIGGER tgSetStatus_1
+before UPDATE ON student -- nếu đổi before thành after thì sẽ không chạy được
+FOR EACH ROW
+
+BEGIN
+	SET new.Status=if(NEW.Age<30,'Young','Old');
+END;
+//DELIMITER ;
+
+update Student set age = 35
+where RN = 4;
+select * from Student;
+
+-- cau 14 
+drop view if exists view_1;
+CREATE VIEW view_1 as
+SELECT s.SName ,t.TName ,st.Mark
+FROM Student as s 
+		LEFT JOIN studenttest as st on s.Rn=st.RN 
+        LEFT JOIN Test as t on st.TestId =t.TestID;
+        
+DELIMITER //
+DROP PROCEDURE IF EXISTS spViewStatus //
+CREATE PROCEDURE spViewStatus(IN Name1 NVARCHAR(20),IN Name2 NVARCHAR(20),OUT output1 nvarchar(50),OUT output2 int)
+	BEGIN
+		 DECLARE diem float;
+		IF Name1 not in( SELECT SName FROM Student) or Name2 not in (SELECT TName FROM Test) 
+ 			THEN 
+ 				SET output1='Không tìm thấy';
+ 		ELSE 
+			SELECT Mark INTO diem FROM view_1 WHERE view_1.SName=Name1 and view_1.TName=Name2;
+            set  output2 = diem;
+ 			IF diem>=5 then
+				SET output1='Đỗ';
+			ELSEIF diem<5 then
+				SET output1='Trượt';
+			ELSE
+				SET output1='Chưa thi';
+			end if;
+ 		end if;
+	END 
+//delimiter ;
+
+CALL spViewStatus('Tuan Minh','SQL1',@a,@b);
+SELECT @a as 'Trạng thái ',@b as 'Điểm';
